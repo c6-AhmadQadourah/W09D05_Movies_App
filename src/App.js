3f9ccc3d92757bf72daa18ba4acaf50e61bb522a
AhmadQadourah <ahmad.h.qadoura@gmail.com>
2022-09-15 23:47:09 +0300

    123

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import HomeList from "./Components/HomeList";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Movie App</h1>
+      </header>
+      <div>
+         <HomeList/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
